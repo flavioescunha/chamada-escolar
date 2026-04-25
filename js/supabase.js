42bc9fcb25dd04f19239d45b812510e31b51356d
Flavio Cunha <flavioescunha@gmail.com>
2026-04-25 11:01:33 -0300

    Primeira versão para produção

---
+++ js/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+const SUPABASE_URL = "https://oweypxjtsetydymatlwu.supabase.co/";
+const SUPABASE_ANON_KEY = "sb_publishable_RZwCNQIZBMnthYeVy8CKjQ_rzLzww6G";
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
